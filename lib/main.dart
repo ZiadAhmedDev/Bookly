c5700396d0ec59ca291793c2ec0d34ac45408dd8
Ziad Ahmed <vet19100108@stud.vet.cu.edu.eg>
2023-03-08 11:58:37 +0200

    bookly app first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bookly/Features/splash/Presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'constants.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData().copyWith(scaffoldBackgroundColor: kPrimeColor),
+        home: const SplashView());
+  }
+}
