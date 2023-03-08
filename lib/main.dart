79c38af9fba05ecb9d080cd92841bafd9063f54c
Ziad Ahmed <vet19100108@stud.vet.cu.edu.eg>
2023-03-08 12:04:31 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bookly/Features/splash/Presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-
 import 'constants.dart';
 
 void main() {
